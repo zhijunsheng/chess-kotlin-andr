500c626ed69f649b99e8910385a1e5b875f14f0d
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-08 14:47:59 -0500

    wrote sample code for "for loop" and "if"

--- Chess/app/src/main/java/com/goldenthumb/android/chess/MainActivity.kt
+++ Chess/app/src/main/java/com/goldenthumb/android/chess/MainActivity.kt
@@ -2,10 +2,19 @@ package com.goldenthumb.android.chess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 0..7) {
+            if (i % 2 == 0) {
+                Log.d(TAG, "i = $i")
+            }
+        }
     }
 }
