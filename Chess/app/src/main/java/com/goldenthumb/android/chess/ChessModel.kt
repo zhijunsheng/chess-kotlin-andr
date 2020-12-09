bcd23287a5b37cb59a52b6ea88f950b72182b8f7
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-08 23:45:21 -0500

    created function pieceAt(..) and finished toString() in ChessModel

--- Chess/app/src/main/java/com/goldenthumb/android/chess/ChessModel.kt
+++ Chess/app/src/main/java/com/goldenthumb/android/chess/ChessModel.kt
@@ -5,6 +5,16 @@ class ChessModel {
 
     init {
         piecesBox.add(ChessPiece(0,0,ChessPlayer.WHITE, ChessRank.ROOK))
+        piecesBox.add(ChessPiece(0,7,ChessPlayer.BLACK, ChessRank.ROOK))
+    }
+
+    private fun pieceAt(col: Int, row: Int) : ChessPiece? {
+        for (piece in piecesBox) {
+            if (col == piece.col && row == piece.row) {
+                return  piece
+            }
+        }
+        return null
     }
 
     override fun toString(): String {
@@ -12,7 +22,33 @@ class ChessModel {
         for (row in 7 downTo 0) {
             desc += "$row"
             for (col in 0..7) {
-                desc += " ."
+                val piece = pieceAt(col, row)
+                if (piece == null) {
+                    desc += " ."
+                } else {
+                    val white = piece.player == ChessPlayer.WHITE
+                    desc += " "
+                    desc += when (piece.rank) {
+                        ChessRank.KING -> {
+                            if (white) "k" else "K"
+                        }
+                        ChessRank.QUEEN -> {
+                            if (white) "q" else "Q"
+                        }
+                        ChessRank.BISHOP -> {
+                            if (white) "k" else "K"
+                        }
+                        ChessRank.ROOK -> {
+                            if (white) "r" else "R"
+                        }
+                        ChessRank.KNIGHT -> {
+                            if (white) "n" else "N"
+                        }
+                        ChessRank.PAWN -> {
+                            if (white) "p" else "P"
+                        }
+                    }
+                }
             }
             desc += "\n"
         }
