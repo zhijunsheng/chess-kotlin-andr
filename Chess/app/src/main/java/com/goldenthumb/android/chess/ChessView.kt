713b78bded89eb3ad35ea8e2947c927206a9ba60
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-11 11:03:51 -0500

    limited screen orientation to portrait only; added a RESET button under chessboard

--- Chess/app/src/main/java/com/goldenthumb/android/chess/ChessView.kt
+++ Chess/app/src/main/java/com/goldenthumb/android/chess/ChessView.kt
@@ -44,6 +44,12 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         loadBitmaps()
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val smaller = min(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(smaller, smaller)
+    }
+
     override fun onDraw(canvas: Canvas?) {
         canvas ?: return
 
